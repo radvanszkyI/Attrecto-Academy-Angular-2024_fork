199bd045df5b6c0dab61080925364363ff0637df
Tóth Tamás <tamas.toth@attrecto.com>
2024-02-19 11:22:54 +0100

    feature/step-1 - add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
 })

104ccd9a4ae2552fe30eb57bf3a3fed33950ab01
radvanszkyI <45069028+radvanszkyI@users.noreply.github.com>
2024-04-08 08:22:57 +0200

    vmi

--- src/app/pages/badges/Services/badge.service.ts
+++ src/app/pages/badges/Services/badge.service.ts
@@ -1,17 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Badge } from '../classes/Badge';
 import { environment } from 'src/environments/environment';
+import { Badge } from '../classes/Badge';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BadgeService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getBadges():Observable<Badge[]>{
+  getBadges(): Observable<Badge[]> {
     return this.httpClient.get<Badge[]>(`${environment.apiUrl}/badges`)
+  }
+
+
+  getBadge(badgeId: number): Observable<Badge> {
+    return this.httpClient.get<Badge>(`${environment.apiUrl}/badges/${badgeId}`)
+  }
+  createBadge(badge: Badge) {
+    return this.httpClient.post(`${environment.apiUrl}/badges/`, badge)
+  }
+  updateBadges(id: number, badge: Badge) {
+    return this.httpClient.put(`${environment.apiUrl}/badges/${id}`, badge)
+  }
+
+  deleteBadges(id: number) {
+    return this.httpClient.delete(`${environment.apiUrl}/badges/${id}`)
   }
 } 
